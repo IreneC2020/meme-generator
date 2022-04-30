e257786910dc9a802a64e4dc85bc5b36cba16164
Irene C <irenec0101@outlook.com>
2022-04-29 22:36:57 -0700

    add data

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,18 @@
+import memesData from "./memesData.js"
+
 export default function Meme(){
+    function getMemesImg() {
+        const memesArray = memesData.data.memes
+        const randomInt = Math.floor(Math.random() * memesArray.length)
+        console.log(memesArray[randomInt].url)        
+    }
     return(
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" placeholder="Top text" className="form-input" />
                 <input type="text" placeholder="Bottom text"className="form-input" />
-                <button className="form-button">Get new meme image</button>
-            </form>
+                <button className="form-button" onClick={getMemesImg}>Get new meme image</button>
+            </div>
         </main>
     )
 
